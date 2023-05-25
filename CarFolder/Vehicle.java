2cfc1d4bb449025ef0c3960b1064bdb426a2a839
Elaine Victoria <111344174+walts2@users.noreply.github.com>
2023-05-26 02:50:30 +0800

    Update and rename Sampletext.html to Vehicle.java

---
+++ CarFolder/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+	String speed = "170 kmph.";
+	String color = "Silver Metallic";
+	String price = "₱706,000";
+	
+	void stop(){
+		System.out.println("Lose Acceleration");
+	}
+}
